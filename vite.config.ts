6a63c1e215a297561810a0e61e5a47781a6e0e95
Ege Nerse <ege@delicious-data.com>
2023-11-04 12:44:57 +0100

    working tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-
-import { defineConfig } from 'vitest/config';
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -11,6 +11,6 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom'
   },
 });
